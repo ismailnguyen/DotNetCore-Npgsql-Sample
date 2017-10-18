5c49352c361262ff22e02a41bf8c795b8f1a5335
ismailnguyen <nguyen.ismail@gmail.com>
2017-10-18 14:43:02 +0200

    Query to find all candidates

--- CandidateRepository.cs
+++ CandidateRepository.cs
@@ -22,5 +22,29 @@ namespace dotnetcore_postgresql_sample
 
             return connection;
         }
+
+        public IEnumerable<Candidate> FindAll()
+        {
+            using (var connection = OpenConnection())
+            {
+                using (var command = connection.CreateCommand())
+                {
+                    var query = "select * from candidates";
+                    command.CommandText = query;
+
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader != null)
+                        {
+                            yield return new Candidate()
+                            {
+                                Id = reader.GetInt32(0),
+                                Email =  reader.GetString(1)
+                            };
+                        }
+                    }
+                }
+            }
+        }
     }
 }
