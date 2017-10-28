73d97e1046da168a406965298a793fcc685c3d7a
ismailnguyen <nguyen.ismail@gmail.com>
2017-10-28 13:50:01 +0200

    Add method to find by email

--- CandidateRepository.cs
+++ CandidateRepository.cs
@@ -46,5 +46,38 @@ namespace dotnetcore_postgresql_sample
                 }
             }
         }
+
+        public IEnumerable<Candidate> Find(string email)
+        {
+            using (var connection = OpenConnection())
+            {
+                using (var command = connection.CreateCommand())
+                {
+                    var query = "select * from candidates where email like @email";
+                    command.CommandText = query;
+
+                    var emailParameter = new NpgsqlParameter("@email", email);
+
+                    command.Parameters.Add(emailParameter);
+
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader != null)
+                        {
+                            yield return new Candidate()
+                            {
+                                Id = reader.GetInt32(0),
+                                Email =  reader.GetString(1)
+                            };
+                        }
+                    }
+                }
+            }
+        }
+
+        public void Insert(string email)
+        {
+            
+        }
     }
 }
